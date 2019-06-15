c14721dc3b9199a4cdcdb67974fa5349ad90bcea
Kazuki Imamura <kaizumaki@gmail.com>
2019-06-15 20:51:51 +0900

    ルートパスをReact Routerに対応

--- config/urls.py
+++ config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from rest_framework import routers
 from dialogue import views
@@ -26,7 +26,7 @@ router.register('entities', views.EntityViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
-    path('', include('frontend.urls')),
+    re_path('^(?:.*)/?$', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
