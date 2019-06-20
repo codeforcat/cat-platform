c923c4d812ef68b47a81267987a5feb89888b6c4
Kazuki Imamura <kaizumaki@gmail.com>
2019-06-20 16:29:34 +0900

    backgroud-colorを設定

--- frontend/src/components/pages/Dialogue/Dialogue.jsx
+++ frontend/src/components/pages/Dialogue/Dialogue.jsx
@@ -1,35 +1,52 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import QAList from '../../organisms/QAList/QAList.jsx';
 import QAEntryContainer from '../../../containers/QAEntryContainer/QAEntryContainer';
 import EntityList from '../../organisms/EntityList/EntityList.jsx';
 import EntityEntry from '../../organisms/EntityEntry/EntityEntry.jsx';
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiInputBase: {
+      input: {
+        background: "#fff",
+        padding: ".5em"
+      },
+      inputMultiline: {
+        padding: ".5em"
+      },
+    },
+  },
+});
+
 export default function Dialogue() {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Ｑ＆Ａ一覧</Link>
-          </li>
-          <li>
-            <Link to="/qa_entry">Ｑ＆Ａ入力修正</Link>
-          </li>
-          <li>
-            <Link to="/entity_list/">Entity一覧</Link>
-          </li>
-          <li>
-            <Link to="/entity_entry">Entity入力修正</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/" exact component={QAList}/>
-        <Route path="/qa_entry/" component={QAEntryContainer}/>
-        <Route path="/entity_list/" component={EntityList}/>
-        <Route path="/entity_entry/" component={EntityEntry}/>
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Ｑ＆Ａ一覧</Link>
+            </li>
+            <li>
+              <Link to="/qa_entry">Ｑ＆Ａ入力修正</Link>
+            </li>
+            <li>
+              <Link to="/entity_list/">Entity一覧</Link>
+            </li>
+            <li>
+              <Link to="/entity_entry">Entity入力修正</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={QAList}/>
+          <Route path="/qa_entry/" component={QAEntryContainer}/>
+          <Route path="/entity_list/" component={EntityList}/>
+          <Route path="/entity_entry/" component={EntityEntry}/>
+        </Switch>
+      </Router>
+    </MuiThemeProvider>
   );
 }
