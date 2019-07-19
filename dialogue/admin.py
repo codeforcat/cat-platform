60db725ffb3b3265d39edf86c3cb29be250b7d13
Kazuki Imamura <kaizumaki@gmail.com>
2019-07-19 21:53:36 +0900

    admin画面修正

--- dialogue/admin.py
+++ dialogue/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Question, Phrase, Entities, Answer, Entity, EntityValue, Synonym
+from .models import Question, Entity
 
 admin.site.register(Question)
+admin.site.register(Entity)
