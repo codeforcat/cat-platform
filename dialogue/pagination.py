7629b1e3baa44c57a261844bcb3629d3ddb02acb
Kazuki Imamura <kaizumaki@gmail.com>
2019-07-31 09:08:26 +0900

    django側のページネーションの不具合を修正

--- dialogue/pagination.py
+++ dialogue/pagination.py
@@ -7,10 +7,10 @@ class CustomPageNumber(pagination.PageNumberPagination):
     page_size = 20
 
     def get_paginated_response(self, data):
+        divPage = divmod(self.page.paginator.count, self.page_size)
         return Response(OrderedDict([
             ('lastPage', self.page.paginator.count),
-            ('countLastPage',
-             self.page_size if self.page.paginator.count % self.page_size == 0 else self.page.paginator.count % self.page_size),
+            ('countLastPage', self.page_size if divPage[0] >= self.page.number else divPage[1]),
             ('current', self.page.number),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
