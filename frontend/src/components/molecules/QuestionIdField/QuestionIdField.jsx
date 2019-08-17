83f8a5be443aaf3d4bacdcb387be666468b45898
Kazuki Imamura <kaizumaki@gmail.com>
2019-08-17 09:31:14 +0900

    Question IDフィールドを非表示に

--- frontend/src/components/molecules/QuestionIdField/QuestionIdField.jsx
+++ frontend/src/components/molecules/QuestionIdField/QuestionIdField.jsx
@@ -12,19 +12,7 @@ export default function QuestionIdField(props) {
   },[props.params]);
 
   return (
-    <FormItem
-      label="question-id"
-      titleText="Question ID"
-      titleAlign="center"
-    >
-      <Input
-        id="question-id"
-        aria-describedby="question-id-helper-text"
-        value={props.params ? props.params : props.id || ''}
-        onChange={(e) => props.actions.inputQuestionId(e.target.value)}
-        onKeyUp={(e) => {if(e.key === 'Enter' && e.target.value !== '') props.actions.setDialogueState(e.target.value)}}
-        disabled
-      />
-    </FormItem>
+    <>
+    </>
   );
 }
