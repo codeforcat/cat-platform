da17839129eaf865fd706e926e3f1cabd8b06370
Kazuki Imamura <kaizumaki@gmail.com>
2019-08-17 09:34:42 +0900

    Entity IDフィールドを非表示に

--- frontend/src/components/molecules/EntityIdField/EntityIdField.jsx
+++ frontend/src/components/molecules/EntityIdField/EntityIdField.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import Input from '@material-ui/core/Input';
-import FormItem from '../FormItem/FormItem.jsx';
 
 export default function EntityIdField(props) {
   useEffect(() => {
@@ -12,19 +10,7 @@ export default function EntityIdField(props) {
   },[props.params]);
 
   return (
-    <FormItem
-      label="entity-id"
-      titleText="Entity ID"
-      titleAlign="center"
-    >
-      <Input
-        id="entity-id"
-        aria-describedby="entity-id-helper-text"
-        value={props.params ? props.params : props.id || ''}
-        onChange={(e) => props.actions.inputEntityId(e.target.value)}
-        onKeyUp={(e) => {if(e.key === 'Enter' && e.target.value !== '') props.actions.setEntityState(e.target.value)}}
-        disabled
-      />
-    </FormItem>
+    <>
+    </>
   );
 }
